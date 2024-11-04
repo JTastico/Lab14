43b9ac715ca084e46843231bb7898c684165fe05
Laboratorio PFR <jamilturpoarocutipa@gmail.com>
2024-11-04 18:56:38 -0500

    varias cosas con error

--- app/src/main/java/com/example/lab14/MainActivity.kt
+++ app/src/main/java/com/example/lab14/MainActivity.kt
@@ -1,43 +1,25 @@
 package com.example.lab14
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lab14.ui.theme.Lab14Theme
+import androidx.navigation.compose.rememberNavController
+import com.example.lab14.navigation.AppNavigator
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Lab14Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            MaterialTheme {
+                val navController = rememberNavController()
+                AppNavigator(navController = navController)
+
+                // Verifica si el intent contiene la redirección
+                val destino = intent.getStringExtra("destino")
+                if (destino == "lista_recetas") {
+                    navController.navigate("lista_recetas")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab14Theme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/lab14/SimpleWidget.kt
+++ app/src/main/java/com/example/lab14/SimpleWidget.kt
@@ -5,4 +5,6 @@ import androidx.glance.appwidget.GlanceAppWidgetReceiver
 
 class SimpleWidget : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = SimpleWidgetContent()
+
+
 }
